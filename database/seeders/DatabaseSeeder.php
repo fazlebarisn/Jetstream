8b929688ad7bffc2786796bd839cd68ba4bf35e0
Fazle Bari <fazlebarisn@gmail.com>
2023-11-16 18:21:59 +0600

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,22 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $admin_role = Role::create(['name' => 'admin']);
+        $admin = User::create([
+            'name'      => 'Admin',
+            'email'     => 'admin@admin.com',
+            'password'  => bcrypt('password')
+        ]);
+        $admin->assignRole($admin_role);
+
+        $user_role = Role::create(['name' => 'user']);
+        $user = User::create([
+            'name'      => 'User',
+            'email'     => 'user@user.com',
+            'password'  => bcrypt('password')
+        ]);
+        $user->assignRole($user_role);
+
+        // $permission = Permission::create(['name' => 'edit articles']);
     }
 }
